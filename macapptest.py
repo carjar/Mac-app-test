d47afd239fc551f4672edc3f2a95770a20003932
Jared Carlson <carjar@arizona.edu>
2026-09-10 17:11:24 -0700

    Updated to specify generation location

--- macapptest.py
+++ macapptest.py
@@ -2,9 +2,18 @@
 """
 Testing .app packaging
 """
+import sys
+import os
 
-# Using open() function
-file_path = "test_file.txt"
+# If the file is in a .app or .exe bundle (sys.frozen = true), manually set the dir
+if getattr(sys, 'frozen', False):
+    app_dir = os.path.dirname(sys.executable)
+else:
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+# Setting open() function path
+file_path = os.path.join(app_dir, "test_file.txt")
 
 # Open the file in write mode
 with open(file_path, 'w') as file:
